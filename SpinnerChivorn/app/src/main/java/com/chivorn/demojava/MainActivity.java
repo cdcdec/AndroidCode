19ca10e4880424e8bd646ad0d1e2416ceead8dd4
cdc <420209677@qq.com>
2019-08-08 08:21:59 +0800

    mqtt

---
+++ SpinnerChivorn/app/src/main/java/com/chivorn/demojava/MainActivity.java
@@ -0,0 +1,130 @@
+package com.chivorn.demojava;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import androidx.core.content.ContextCompat;
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner;
+
+public class MainActivity<T> extends MainApp<T> {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.smart_material_spinner_sample_layout);
+
+        initBaseView();
+        initItemList();
+        initSpinnerInJava();
+        onClickListener();
+    }
+
+    private void initSpinnerInJava() {
+        spSearchable.setItem(provinceList);
+        spReSelectable.setItem(provinceList);
+        spProvince.setItem(provinceList);
+        spProvinceNoHint.setItem(provinceList);
+        spProvinceDialog.setItem(provinceList);
+        spCustomColor.setItem(provinceList);
+
+        spSearchable.setSearchDialogGravity(Gravity.TOP);
+        spSearchable.setErrorTextAlignment(SmartMaterialSpinner.TextAlignment.ALIGN_LEFT);
+        spSearchable.setArrowPaddingRight(19);
+        spCustomColor.setItemColor(ContextCompat.getColor(this, R.color.custom_item_color));
+        spCustomColor.setSelectedItemListColor(ContextCompat.getColor(this, R.color.custom_selected_item_color));
+        spCustomColor.setItemListColor(ContextCompat.getColor(this, R.color.custom_item_list_color));
+
+        setOnEmptySpinnerClickListener(spEmptyItem);
+        setOnItemSelectedListener(spSearchable, spReSelectable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor, spEmptyItem, spVisibilityChanged);
+        setOnSpinnerEventListener(spSearchable, spReSelectable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor, spEmptyItem);
+        // setItemTextSize(90, spSearchable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor, spEmptyItem);
+        //  setErrorTextSize(90, spSearchable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor, spEmptyItem);
+        //  setSelection(3, spSearchable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor);
+        // clearSelection(spSearchable, spProvince, spProvinceNoHint, spProvinceDialog, spCustomColor);
+    }
+
+    private void setOnItemSelectedListener(SmartMaterialSpinner... spinners) {
+        for (final SmartMaterialSpinner spinner : spinners) {
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    if (spinner.getId() == R.id.sp_searchable) {
+                        spinner.setErrorText("Your selected item is \"" + spinner.getItem().get(position) + "\" .");
+                    } else if (spinner.getId() == R.id.sp_reselectable || spinner.getId() == R.id.sp_visibility_changed) {
+                        Toast.makeText(MainActivity.this, "Selected item is: " + spinner.getSelectedItem(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        spinner.setErrorText("You are selecting on spinner item -> \"" + spinner.getItem().get(position) + "\" . You can show it both in XML and programmatically and you can display as single line or multiple lines");
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                    spinner.setErrorText("On Nothing Selected");
+                }
+            });
+        }
+    }
+
+    private void setOnSpinnerEventListener(SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.setOnSpinnerEventListener(new SmartMaterialSpinner.OnSpinnerEventListener() {
+                @Override
+                public void onSpinnerOpened(SmartMaterialSpinner spinner) {
+                    Log.i("SpinnerEventListener", "onSpinnerOpened: ");
+                }
+
+                @Override
+                public void onSpinnerClosed(SmartMaterialSpinner spinner) {
+                    Log.i("SpinnerEventListener", "onSpinnerClosed: ");
+                }
+            });
+        }
+    }
+
+    private void setOnEmptySpinnerClickListener(SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.setOnEmptySpinnerClickListener(new SmartMaterialSpinner.OnEmptySpinnerClickListener() {
+                @Override
+                public void onEmptySpinnerClicked() {
+                    Toast.makeText(MainActivity.this, getString(R.string.empty_item_spinner_click_msg), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void setItemTextSize(float size, SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.setItemSize(size);
+        }
+    }
+
+    private void setErrorTextSize(int size, SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.setErrorTextSize(size);
+        }
+    }
+
+    private void setSelection(int index, SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.setSelection(index);
+        }
+    }
+
+    private void clearSelection(SmartMaterialSpinner... spinners) {
+        for (SmartMaterialSpinner spinner : spinners) {
+            spinner.clearSelection();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        if (v.getId() == R.id.btn_goto_runtime_render) {
+            intent = new Intent(getApplicationContext(), RuntimeRenderViewActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ SpinnerChivorn/app/src/main/java/com/chivorn/demojava/RuntimeRenderViewActivity.java
@@ -0,0 +1,96 @@
+package com.chivorn.demojava;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner;
+
+import java.util.List;
+
+
+public class RuntimeRenderViewActivity<T> extends MainApp<T> {
+    private LinearLayout layoutContainer;
+    private SparseArray<List<String>> itemListMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_runtime_render_view_activity);
+
+        initViewObject();
+        initItemList();
+        initItemListMap();
+        renderView();
+    }
+
+    private void initViewObject() {
+        layoutContainer = findViewById(R.id.dynamic_fields_layout);
+    }
+
+    private void initItemListMap() {
+        itemListMap = new SparseArray<>();
+        itemListMap.put(0, countryList);
+        itemListMap.put(1, countryList.subList(0, (countryList.size() - 1) / 2));
+        itemListMap.put(2, countryList.subList(0, 200));
+        itemListMap.put(3, countryList.subList(0, 100));
+        itemListMap.put(4, countryList.subList(0, 50));
+        itemListMap.put(5, countryList.subList(0, 1));
+    }
+
+    private void renderView() {
+        for (int i = 0; i < 6; i++) {
+            final SmartMaterialSpinner<String> smsp = new SmartMaterialSpinner<>(this);
+            smsp.setReSelectable(true);
+            smsp.setHintColor(Color.GRAY);
+            smsp.setUnderlineColor(Color.GRAY);
+            smsp.setErrorText("This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. ");
+            smsp.setItem(itemListMap.get(i));
+            smsp.setHint("Spinner " + (i + 1));
+
+            if (smsp.getCount() >= 100) {
+                smsp.setSearchable(true);
+            }
+
+            smsp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(getApplicationContext(), "You selected on " + smsp.getSelectedItem(), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    Log.i("AAA", "onNothingSelected: ");
+                }
+            });
+            layoutContainer.addView(smsp);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            final SmartMaterialSpinner<String> smsp = new SmartMaterialSpinner<>(this);
+            smsp.setReSelectable(true);
+            smsp.setHintColor(Color.GRAY);
+            smsp.setUnderlineColor(Color.GRAY);
+            smsp.setErrorText("This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. ");
+            smsp.setItem(itemListMap.get(i));
+            smsp.setHint("Normal Spinner " + (i + 1));
+            layoutContainer.addView(smsp);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            final SmartMaterialSpinner<String> smsp = new SmartMaterialSpinner<>(this);
+            smsp.setReSelectable(true);
+            smsp.setHintColor(Color.GRAY);
+            smsp.setUnderlineColor(Color.GRAY);
+            smsp.setErrorText("This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. This is error text. Hello Cambodia Kingdom of Wonder. ");
+            smsp.setMultilineError(false);
+            smsp.setItem(itemListMap.get(i));
+            smsp.setHint("Single Line Error Spinner " + (i + 1));
+            layoutContainer.addView(smsp);
+        }
+    }
+}
